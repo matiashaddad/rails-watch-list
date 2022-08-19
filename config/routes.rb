0b3896804aa9f0276a43808a59076f8a4f4e6e7e
matiashaddad <matiashaddad@outlook.de>
2022-08-19 16:47:35 +0200

    wl

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "lists#index"
   resources :lists, only: [:index, :show, :new, :create, :edit, :update] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: [:new, :create]
